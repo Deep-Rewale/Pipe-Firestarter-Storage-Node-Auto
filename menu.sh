f948c40218742a67ff978e52f7ab00d3b3f5ac15
Deep-Rewale <67994719+Deep-Rewale@users.noreply.github.com>
2025-08-20 22:35:45 +0530

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -20,7 +20,7 @@ show_header() {
     echo "██  ██  ██ ██   ██ ██   ██ ██          ██   ██    ██        ██   ██ ██      ██      ██      "
     echo "██      ██ ██   ██ ██████  ███████     ██████     ██        ██████  ███████ ███████ ██      "
     echo "                                                                                            "
-    echo -e "${YELLOW}           🚀 Gensyn RL-Swarm Launcher Made By Deep 🚀${NC}"
+    echo -e "${YELLOW}             🚀 PIPE NODE MANAGER BY DEEP 🚀${NC}"
     echo -e "${YELLOW}            💻 GitHub: https://github.com/Deep-Rewale${NC}"
     echo -e "${YELLOW}            💬Telegram: https://t.me/Deeprewale${NC}"
     echo -e "${YELLOW}           💡 X ACCOUNT: https://x.com/deep_rewale28${NC}"
